57759981f81ab2c43ac1d6341691a3e0d5382b40
Jiwon-Hwang <54762704+Jiwon-Hwang@users.noreply.github.com>
2020-03-06 02:35:06 +0900

    import 에러 해결, 서버로 받은 이미지 출력#45

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 import uuid
-from facemakeup import segmentation
+from flaskr.facemakeup.segmentation import segmentation
 
 UPLOAD_FOLDER = 'static/images'
 if platform.system() == 'Windows':
@@ -17,6 +17,7 @@ app = Flask(__name__, instance_relative_config=True,
             static_url_path='/static')
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 
 @app.route('/')
@@ -53,7 +54,7 @@ def upload_file():
             filename = 'face.jpg'
             image_path = os.path.join(path, filename)
             f.save(image_path)
-            segmentation(image_path)
+            segmentation(path)
             return id
 
         return 'fail'
--- flaskr/facemakeup/segmentation.py
+++ flaskr/facemakeup/segmentation.py
@@ -1,2 +1,8 @@
-def segmetation(path):
-    print(path)
+import numpy as np
+import cv2 as cv2
+import os
+def segmentation(path):
+    img = cv2.imread(os.path.join(path, 'face.jpg'))
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
