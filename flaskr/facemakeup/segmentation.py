bf8ecdceae9bbc51f83bfb116a191385b62cdf17
ParkEunwoo <dmsdn960@gmail.com>
2020-03-08 16:57:04 +0900

    initial commit

---
+++ flaskr/facemakeup/segmentation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2 as cv2
+import os
+from flaskr.facemakeup.test import evaluate
+from flaskr.facemakeup.makeup import makeup
+
+def segmentation(path):
+    current_dir = os.path.dirname(os.path.abspath(__file__)) #절대경로
+    image_path = os.path.join(path, 'face.jpg')
+    cp = os.path.join(current_dir, 'cp/79999_iter.pth') #상대경로->절대경로로
+
+    img = cv2.imread(image_path) #우선 이미지 읽어들이기
+    ori = img.copy() #original
+    parsing = evaluate(image_path, cp) # test.py의 evaluate() 함수. (512, 512)의 image (parsing) return.
+    parsing = cv2.resize(parsing, img.shape[0:2], interpolation=cv2.INTER_NEAREST) #이미지 사이즈 조정. 픽셀사이 값 보간. / image.shape[] : [Y축, X축, 채널수] 순서 중 0,1번지 2개만 가져옴
+    
+    parsing_image_path = os.path.join(path, 'parsing.jpg')
+    cv2.imwrite(parsing_image_path, parsing)
+
+    makeup_img = makeup(image_path, parsing_image_path)
+
+    cv2.imshow('ori', cv2.resize(ori, (512,512))) #original img
+    cv2.imshow('seg', cv2.resize(parsing.astype('uint8'), (512,512))) #segmentation img
+    #print(np.shape(parsing), type(parsing)) #(960, 960), <class 'numpy.ndarray'>
+    cv2.imshow('makeup', cv2.resize(makeup_img.astype('uint8'), (512,512)))
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
---
+++ flaskr/facemakeup/test.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import torch
+import os
+from flaskr.facemakeup.model import BiSeNet
+import os.path as osp
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+import cv2
+
+
+def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
+    # Colors for all 20 parts
+    part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
+                   [255, 0, 85], [255, 0, 170],
+                   [0, 255, 0], [85, 255, 0], [170, 255, 0],
+                   [0, 255, 85], [0, 255, 170],
+                   [0, 0, 255], [85, 0, 255], [170, 0, 255],
+                   [0, 85, 255], [0, 170, 255],
+                   [255, 255, 0], [255, 255, 85], [255, 255, 170],
+                   [255, 0, 255], [255, 85, 255], [255, 170, 255],
+                   [0, 255, 255], [85, 255, 255], [170, 255, 255]]
+
+    im = np.array(im)
+    vis_im = im.copy().astype(np.uint8)
+    vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
+    vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
+    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+
+    num_of_class = np.max(vis_parsing_anno)
+
+    for pi in range(1, num_of_class + 1):
+        index = np.where(vis_parsing_anno == pi)
+        vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
+
+    vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
+    # print(vis_parsing_anno_color.shape, vis_im.shape)
+    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
+
+    # Save result or not
+    if save_im:
+        cv2.imwrite(save_path[:-4] +'.png', vis_parsing_anno)
+        cv2.imwrite(save_path, vis_im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    return vis_parsing_anno
+    # return vis_im
+
+# 학습시키기(학습시켜놓은걸로 평가하기)
+def evaluate(image_path, cp):
+
+    # if not os.path.exists(respth):
+    #     os.makedirs(respth)
+
+    n_classes = 19
+    net = BiSeNet(n_classes=n_classes) # BiSeNet으로 net이리는 인스턴스 생성됨. 인자로 19 넣어서 만듦.
+    net.cuda() # Tensor들을 GPU로 보내기
+    net.load_state_dict(torch.load(cp))
+    net.eval()
+
+    to_tensor = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+
+    with torch.no_grad():
+        img = Image.open(image_path)
+        image = img.resize((512, 512), Image.BILINEAR)
+        img = to_tensor(image)
+        img = torch.unsqueeze(img, 0)
+        
+        img = img.cuda()
+        out = net(img)[0]
+        parsing = out.squeeze(0).cpu().numpy().argmax(0)
+        # print(np.shape(img),np.shape(parsing),parsing) => torch.Size([1,3,512, 512]) / (512, 512) / [0 0 0...0 0 0]~[17 17 17... 17 17 17]
+        # print(np.unique(parsing)) 
+
+        # vis_parsing_maps(image, parsing, stride=1, save_im=False, save_path=osp.join(respth, dspth))
+        return parsing
+
+if __name__ == "__main__":
+    #evaluate(dspth='/home/zll/data/CelebAMask-HQ/test-img/116.jpg', cp='79999_iter.pth')
+    evaluate('./imgs/116.jpg', cp='cp/79999_iter.pth') #이렇게 바꾸면 오류는 해결. line74 주석처리하면 아무것도 출력 X
+
+
